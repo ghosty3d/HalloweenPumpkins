23dd744e1e3f2c41392eb39a6b369d4cad2159a6
Ghosty3d <ghost201087@gmail.com>
2015-10-28 04:52:18 +0200

    Bomb

---
+++ HalloweenPumpkins/Assets/Scripts/Enemies/BombManger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class BombManger
+{
+	public static int BombsCount;
+
+	public static void SetStartBombsCount(int bombs)
+	{
+		BombsCount = bombs;
+		GameManager.Instance.GameViewUI.UpdateBombsCount (BombsCount);
+	}
+
+	public static void AddBombs(int bombs)
+	{
+		BombsCount += bombs;
+		GameManager.Instance.GameViewUI.UpdateBombsCount (BombsCount);
+	}
+
+	public static void RemoveBombs(int bombs)
+	{
+		if (BombsCount > 0)
+		{
+			BombsCount -= bombs;
+			GameManager.Instance.GameViewUI.UpdateBombsCount (BombsCount);
+		}
+		else
+		{
+			GameManager.Instance.GameViewUI.HideBombContainer ();
+		}
+	}
+}
---
+++ HalloweenPumpkins/Assets/Scripts/Enemies/BombObject.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombObject : MonoBehaviour
+{
+	public SphereCollider DamageZone;
+	public float RotationSpeed = 20f;
+	public float BombTimer;
+	public float BombRadius;
+	private bool explosed = false;
+
+	void OnEnable ()
+	{
+		StartCoroutine (BadaBoom(BombTimer));
+		DamageZone.enabled = false;
+	}
+
+	void Update()
+	{
+		transform.Rotate (transform.up * RotationSpeed * Time.deltaTime);
+	}
+	
+	IEnumerator BadaBoom(float timer)
+	{
+		while(explosed != true)
+		{
+			yield return new WaitForSeconds (timer);
+			explosed = true;
+			Explose ();
+			Destroy (this.gameObject, 0.1f);
+		}
+	}
+
+	public void Explose()
+	{
+		Debug.Log ("Explose");
+		DamageZone.enabled = true;
+		DamageZone.isTrigger = true;
+		DamageZone.radius = BombRadius;
+
+		//Particles
+		ParticlesManager.Instance.PlaceBombParticles(transform.position);
+		StopCoroutine ("BadaBoom");
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log ("OnTriggerEnter");
+		if(other.CompareTag("Enemy"))
+		{
+			other.gameObject.GetComponent<Enemy> ().Clicked = true;
+			other.gameObject.SetActive (false);
+		}
+	}
+}
