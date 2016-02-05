7bc80697268769e52e99970ab55e527da78a44aa
Dmitriy Martynov <martynov.dmitriy88@gmail.com>
2016-02-05 23:33:43 +0200

    Levels Editor and Serialization class

---
+++ HalloweenPumpkins/Assets/Editor/LevelEditorWindow.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+
+public class LevelEditorWindow : EditorWindow
+{
+    public static string configPath = Application.dataPath + "/Config/level.json";
+
+    public Level newLevel;
+
+    [MenuItem("Levels/LevelsEditor")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        LevelEditorWindow window = (LevelEditorWindow)EditorWindow.GetWindow(typeof(LevelEditorWindow));
+        window.Show();
+
+    }
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Load Config", GUILayout.ExpandWidth(true), GUILayout.Height(32)))
+        {
+           
+            Debug.Log(configPath);
+
+          
+
+           // Level level = new Level(2, 3, 4, 5, 6, true, 10000);
+           // Serializer.Serialize(level, configPath);
+            newLevel = Serializer.Deserialize<Level>(configPath);
+        }
+
+        if (newLevel != null)
+        {
+            newLevel.ID = EditorGUILayout.IntField(string.Format("Level ID:\t\t\t"), newLevel.ID);
+            newLevel.LevelTime = EditorGUILayout.IntField(string.Format("Level Time:\t\t"), newLevel.LevelTime);
+            newLevel.WavesCount = EditorGUILayout.IntField(string.Format("Level WavesCount:\t\t"), newLevel.WavesCount);
+            newLevel.MinEnemyCount = EditorGUILayout.IntField(string.Format("Level MinEnemyCount:\t"), newLevel.MinEnemyCount);
+            newLevel.MaxEnemyCount = EditorGUILayout.IntField(string.Format("Level MaxEnemyCount:\t"),newLevel.MaxEnemyCount);
+            newLevel.isLocked = EditorGUILayout.Toggle(string.Format("Level isLocked:\t\t"), newLevel.isLocked);
+            newLevel.Stars = EditorGUILayout.IntField(string.Format("Level Stars:\t\t"), newLevel.Stars);
+        }
+
+        if (GUILayout.Button("Update Config", GUILayout.ExpandWidth(true), GUILayout.Height(32)))
+        {
+            Serializer.Serialize(newLevel, configPath);
+            AssetDatabase.Refresh();
+        }
+    }
+}
---
+++ HalloweenPumpkins/Assets/Scripts/Serialization/Serializer.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.IO;
+using Newtonsoft.Json;
+using Object = System.Object;
+
+public static class Serializer 
+{
+    private static readonly JsonSerializer serializer;
+
+
+
+    static Serializer()
+    {
+        serializer = new JsonSerializer();
+    }
+
+
+    /// <summary>
+    /// Deserialize using for convertion JSON data file into data class.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    public static T Deserialize<T>(string path)
+    {
+        if (File.Exists(path))
+        {
+            using (StreamReader reader = new StreamReader(path))
+            using (JsonReader jsonReader = new JsonTextReader(reader))
+            {
+                return serializer.Deserialize<T>(jsonReader);
+
+            }
+        }
+        else
+        {
+            Debug.LogError(path + " Not Exist");
+            return default(T);
+        }
+    }
+
+    /// <summary>
+    /// Serialize using for convert object data to config JSON data file.
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <param name="path"></param>
+    public static void Serialize(object obj, string path)
+    {
+        if (!File.Exists(path))
+        {
+            using (StreamWriter sw = File.CreateText(path));
+#if UNITY_EDITOR
+            UnityEditor.AssetDatabase.Refresh();
+#endif
+        }
+
+        using (StreamWriter sw = new StreamWriter(path))
+        using (JsonWriter writer = new JsonTextWriter(sw))
+        {
+            serializer.Serialize(writer, obj);
+        }
+    }
+}
