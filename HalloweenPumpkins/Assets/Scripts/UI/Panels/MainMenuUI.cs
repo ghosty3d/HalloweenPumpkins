4c63d5e0f172155e1610a490682027f4c49470e5
Ghosty3d <ghost201087@gmail.com>
2016-02-11 01:30:16 +0200

    Copyright actual year in the bottom of MainMenu.

--- HalloweenPumpkins/Assets/Scripts/UI/Panels/MainMenuUI.cs
+++ HalloweenPumpkins/Assets/Scripts/UI/Panels/MainMenuUI.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MainMenuUI : MonoBehaviour
 {
 	public static MainMenuUI Instance;
+	public Text copyrightLabel;
 
 	void Awake()
 	{
 		Instance = this;
+		copyrightLabel.text = string.Format ("WTFGames © {0}", System.DateTime.Now.Year);
 	}
 }
